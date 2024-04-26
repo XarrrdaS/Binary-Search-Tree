ec16c0442166e2a6ad8f7628b34cd82c6001b66c
XarrrdaS <k.radomski1226@gmail.com>
2024-04-26 08:51:58 +0200

    rebalance

--- AVL/Operations/rebalance.py
+++ AVL/Operations/rebalance.py
@@ -1,3 +1,17 @@
-def rebalance(root):
-    print('Tree rebalanced successfully!')
+def flatten(root):
+    if not root:
+        return []
+    return flatten(root['left']) + [root['key']] + flatten(root['right'])
+
+def build_balanced(nodes):
+    if not nodes:
+        return None
+    mid = len(nodes) // 2
+    root = {'key': nodes[mid], 'left': None, 'right': None}
+    root['left'] = build_balanced(nodes[:mid])
+    root['right'] = build_balanced(nodes[mid+1:])
     return root
+
+def rebalance(root):
+    nodes = flatten(root)
+    return build_balanced(nodes)
